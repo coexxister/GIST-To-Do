de793843a5a2ac7937f48d8570cfed770dc4a4af
Grzegorz Debicki <debicki.grzegorz@outlook.com>
2021-04-08 00:17:45 +0200

    Add project files.

---
+++ GIST To-Do/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GIST_To_Do.Models;
+
+namespace GIST_To_Do.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
